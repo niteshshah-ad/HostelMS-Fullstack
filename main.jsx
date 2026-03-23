4a0cdb8b43a075a307e247574f25a777f726574b
NiteshRepo <niteshjishah25@gmail.com>
2026-03-24 00:58:32 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import ClickWave from "./components/ClickWave.jsx";
+import ClickWave from "./ClickWave.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
